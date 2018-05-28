95298dbc3cb641409915d48667cd66127f998b10
Jonathan Hosein <jonathan.hosein@live.com>
2018-05-28 18:24:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var recipes = new Object();
 
-function updateObjectWithKeyAndvalue(object, key, value) {
+function updateObjectWithKeyAndValue(object, key, value) {
 
-  object.key = value;
+  object[key] = value;
 
   return object;
 }
