06fdcadb33f723859ae03716b932c99b6335df08
Jonathan Hosein <jonathan.hosein@live.com>
2018-05-28 18:31:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+//function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
-  return Object.Assign({}, object, {key: value});
-}
+//  return object.Assign({}, object, {key: value});
+//}
